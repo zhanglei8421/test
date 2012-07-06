f58208b682d00caaa2ce4a85cd1a9589282484a2
zhanglei <zhanglei8421@126.com>
2012-07-06 15:12:19 +0800

    init the project

---
+++ ver.cpp
@@ -0,0 +1,28 @@
+//---------------------------------------------------------------------------
+
+
+#pragma hdrstop
+
+#include "ver.h"
+
+//---------------------------------------------------------------------------
+
+#pragma package(smart_init)
+
+int GetVersion(WORD &v1, WORD &v2, WORD &v3, WORD &v4)
+{
+    char t[512];
+    GetModuleFileName(NULL, t, 512);
+    if(GetFileVersionInfo(t, 0, 512, t))
+    {
+        VS_FIXEDFILEINFO *pFileInfo;
+        UINT n = 0;
+        VerQueryValue(t, (LPTSTR)("\\"), (void**)&pFileInfo, &n);
+        v1 = HIWORD(pFileInfo->dwFileVersionMS);
+        v2 = LOWORD(pFileInfo->dwFileVersionMS),
+        v3 = HIWORD(pFileInfo->dwFileVersionLS);
+        v4 = LOWORD(pFileInfo->dwFileVersionLS);
+        return 0;
+    }
+    return -1;
+}
